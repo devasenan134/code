b0b3b71891586128f5c08ca8ac4d791649d608be
devasenan134 <devasenan.murugan@gmail.com>
2022-09-06 12:19:06 +0530

    final mail for slot

--- Python/bulk_mailer/mk_mail.py
+++ Python/bulk_mailer/mk_mail.py
@@ -14,33 +14,27 @@ import csv
 sender_email = "bharathipriya.cs@ieee.org"
 sender_passwd = "ieee2022"
 
-def create_content():
+def create_content(content):
     subject = "MAKERFAIR -IEEE YESIST 2022 ABSTRACT SUBMISSION REG"
     content = """\
-Dear YESIST’12 Mentor,
+Good day Infotechs,
 
-Greetings of the Day!!
-Thanks for your active guidance throughout the journey of YESIST’12 2022 for all the budding innovators. Your valuable support to the participants, allowed them to successfully submit their abstracts and projects. This added more value to YESIST’12 with quality submissions. 
+“The only way to discover the limits of the possible is to go beyond the impossible”
 
-We would like to invite you the Grand Finale of YESIST’12 and see your students perform against the innovators from various countries.   
-Why attend the Grand Finale?
+Yup, the wait is over and the presentation for Makers Fair is all set to happen on Sep 10,11 at Sri Venkateswara College of Engineering, Bangalore. 
 
-Opportunity to connect in person with the Keynote speakers and other international guests. 
-Networking with fellow pilots, committee members and mentors. 
-Opportunity to know about the innovation from other countries and interacting with the teams.
+The presentations are set to happen on Sep 10th.
+You have to present (presentation and demo) for a time of 10-12 minutes and Q&A by jury will be for 3 minutes after the presentation.
+Attached are your allotted time slot for the presentation. 
+Slot - {0} IST
 
-For mentors attending the event physically: Please register through the registration link: https://ieeeyesist12.org/pilot-finale-registration/
-A registration fee of Rs.500 is to be paid for mentor attendance. The registration fee includes lunch and refreshments on both days of the event and dinner on 10th. No travel or accommodation will be provided by the organisers. 
+Kindly follow the abstract in the stipulated template and it is advisable to follow up the review also. The prototype should be demonstrated for hardware solution and simulation is required for software solution.Any queries if, kindly revert back to us.
 
-The details of the Grand Finale of YESIST’12 2022 are mentioned below.
-Venue: Sri Venkateshwara College of Engineering, Bengaluru, India.
-Dates: September 10,11, 2022.
-
-(Applicable for the mentors who are interested in joining the event in physical mode)
+Suit up for the big day!
 
 Thanks & Regards,
 Team YESIST’12 2022
-"""
+""".format(content)
 
     return subject, content
 
@@ -49,21 +43,42 @@ Team YESIST’12 2022
 
 
 def process_csv(path):
+    mentors = []
+    slots = []
+    participants = []
+    
+    mentor = ""
+    time = ""
     email = []
+
     with open(path) as file:
         reader = csv.reader(file)
         next(reader)
         for i in reader:
-            if int(i[0]) in [699, 700, 689, 723, 654, 673, 688, 691, 710, 712, 718, 722, 727, 686, 701, 713, 693, 702, 717, 714, 715, 726, 731, 743]:
+            if i[0] != '':
+                slots.append(time)
+                mentors.append(mentor)
+                participants.append(email)
+                email = []
+
+                time = i[1] 
+                mentor = i[13]
                 email.append(i[7])
+            else:
+                email.append(i[7])
+            
+        slots.append(time)
+        mentors.append(mentor)
+        participants.append(email)
 
     # filter_none = lambda mail: filter(None, mail)
     # email = [list(filter_none(mail))for mail in email]
-    return email
+    return mentors, slots, participants
 
 
-def create_msg(email, cc_mail=None, name=None):
-    subject, content = create_content()
+def create_msg(email, time, cc_mail=None):
+    subject, content = create_content(time)
+
     msg = MIMEMultipart()
     msg["From"] = sender_email
     msg["Subject"] = subject
@@ -80,21 +95,26 @@ def create_msg(email, cc_mail=None, name=None):
     return msg
 
 
-def send_mail(receivers):
+def send_mail(mentors, slots, participants):
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(sender_email, sender_passwd)
-        for email in receivers:
-            msg = create_msg(email)
-            server.send_message(msg)
-            print("sent to " + email)
+        for i in range(1, len(slots)):
+            team = participants[i]
+            team.append(mentors[i])
+
+            for mail in team:
+                msg = create_msg(mail, slots[i])
+
+            # server.send_message(msg)
+            
+            print("sent to", team)
 
 
 
 
 
 
-receivers = process_csv("email_list.csv")
-print(receivers, len(receivers))
+mentors, slots, participants = process_csv("MFFinale-Registration.csv")
+print(slots, len(slots))
 
-
-send_mail(receivers)
+send_mail(mentors, slots, participants)
