385d19f7907f2e94e8ae1e324f18039d1919288d
devasenan134 <devasenan.murugan@gmail.com>
2022-11-25 22:34:21 +0530

    finaly done with the jongo

--- Web_Track/moJongo/blog/models.py
+++ Web_Track/moJongo/blog/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    likes = models.IntegerField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- Web_Track/moJongo/blog/views.py
+++ Web_Track/moJongo/blog/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render, HttpResponse
-
+from .forms import BlogForm
+from .models import Blog
 # Create your views here.
 
 def home(request):
     return HttpResponse('Home Page')
+
+def create(request):
+    create_blog_form = BlogForm()
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponse('Blog Published :)')
+    return render(request, 'blog/create.html', {
+        'form': create_blog_form,
+    })
+
+def read(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/read.html', {
+        'blogs': blogs
+    })
+
+def update(request, id):
+    blog = Blog.objects.get(id=id)
+    old_blog = BlogForm(instance=blog)
+    if request.method == 'POST':
+        new_blog = BlogForm(request.POST, instance=blog)
+        if new_blog.is_valid():
+            new_blog.save()
+        return HttpResponse('Blog Update :)')
+    return render(request, 'blog/update.html', {
+        'form': old_blog
+    })
+
+def delete(request, id):
+    blog = Blog.objects.get(id=id)
+    blog.delete()
+    return HttpResponse('Blog Deleted :(')
